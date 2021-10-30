d24b782a4d7916b994ed797cf5d003f6cc4e74f4
Dave Quinn <dquinn8@cogeco.ca>
2021-10-30 14:45:31 -0400

    got server running

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: '',
-  database: 'election'
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Che$gr8r',
+    database: 'election'
 });
 
 module.exports = db;
