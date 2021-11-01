5984d64980078986124fdbf5ef062477c95dd72d
Dave Quinn <dquinn8@cogeco.ca>
2021-11-01 11:47:38 -0400

    voter routes: check

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
-// Get the total votes for all the candidates
+// Get the total votes  for all the candidates
 router.get('/votes', (req, res) => {
-  const sql = `SELECT candidates.*, parties.name AS party_name, 
+    const sql = `SELECT candidates.*, parties.name AS party_name, 
                 COUNT(candidate_id) 
                 AS count FROM votes 
                 LEFT JOIN candidates ON votes.candidate_id = candidates.id 
@@ -13,40 +13,40 @@ router.get('/votes', (req, res) => {
                 GROUP BY candidate_id 
                 ORDER BY count DESC`;
 
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
     });
-  });
 });
 
 // Create a vote record
 router.post('/vote', ({ body }, res) => {
-  const errors = inputCheck(body, 'voter_id', 'candidate_id');
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
-
-  const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
-  const params = [body.voter_id, body.candidate_id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    res.json({
-      message: 'success',
-      data: body,
-      changes: result.affectedRows
+
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
     });
-  });
 });
 
 module.exports = router;
