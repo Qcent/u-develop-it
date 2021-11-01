b00e2f5bdbfd73697945cd8cb762b69064b2b897
Dave Quinn <dquinn8@cogeco.ca>
2021-11-01 11:39:33 -0400

    modularized

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//import all routes
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
